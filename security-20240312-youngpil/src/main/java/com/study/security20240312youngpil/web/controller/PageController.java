527816da48121ea66b3fc2855ff6f8c11a13d305
nobodys5 <155369610+nobodys5@users.noreply.github.com>
2024-03-12 21:58:10 +0900

    Add files via upload

---
+++ security-20240312-youngpil/src/main/java/com/study/security20240312youngpil/web/controller/PageController.java
@@ -0,0 +1,21 @@
+package com.study.security20240312youngpil.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+	@GetMapping({"/","/index"})
+	public String loadIndex() {
+		return "index";
+	}
+	@GetMapping("/auth/signin")
+	public String loadSignin() {
+		return "auth/signin";
+	}
+	@GetMapping("/auth/signup")
+	public String loadSignup() {
+		return "auth/signup";
+	}
+}
