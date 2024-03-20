0ca149c57ad00f3aa8ab2613faaf7f4f3f31b6e7
nobodys5 <155369610+nobodys5@users.noreply.github.com>
2024-03-20 21:44:53 +0900

    Add files via upload

--- security-20240312-youngpil/src/main/java/com/study/security20240312youngpil/service/auth/PrincipalDetails.java
+++ security-20240312-youngpil/src/main/java/com/study/security20240312youngpil/service/auth/PrincipalDetails.java
@@ -3,26 +3,36 @@ package com.study.security20240312youngpil.service.auth;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import com.study.security20240312youngpil.domain.user.User;
 
 import lombok.Data;
 
 @Data
-public class PrincipalDetails implements UserDetails {
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
 	
 	
 	
 	private static final long serialVersionUID = -8490096719701750537L;
 	private User user;
+	
+	private Map<String, Object> attributes;
 
 	public PrincipalDetails(User user) {
 		this.user = user;
 	}
+	
+	public PrincipalDetails(User user, Map<String, Object> attributes) {
+		this.user = user;
+		this.attributes = attributes;
+	}
+	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		
@@ -83,6 +93,15 @@ public class PrincipalDetails implements UserDetails {
 		// 사용자 활성화 여부
 		// true: 활성화, false: 비활성화
 	}
+	@Override
+	public Map<String, Object> getAttributes() {
+		return attributes;
+	}
+	@Override
+	public String getName() {
+		return user.getUser_name();
+	}
+
 
 	
 }
