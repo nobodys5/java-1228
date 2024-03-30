c620aba628f9b3e61e098ed54d40e6482a737a16
nobodys5 <155369610+nobodys5@users.noreply.github.com>
2024-03-30 18:30:00 +0900

    Add files via upload

---
+++ study/js/ppt15-1.js
@@ -0,0 +1,17 @@
+var then = 10;
+var bar = "hello";
+var bar2 = "true";
+var bar3 = ['반가워','친구들','잘가'];
+console.log(then);
+console.log(bar);
+console.log(bar2);
+console.log(bar3);
+
+function add() {
+    var num1 = 10;
+    var num2 = 20;
+    var sum = num1 + num2;
+    alert("결과값:" + sum);
+}
+
+add();
